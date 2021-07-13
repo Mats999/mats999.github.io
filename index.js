98f9c439c777301ee4d79232cde1bcfb19496bc6
Mats999 <77190366+Mats999@users.noreply.github.com>
2021-07-13 18:52:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-L.CursorHandler = L.Handler.extend({
-
-    addHooks: function () {
-        this._popup = new L.Popup();
-        this._map.on('mouseover', this._open, this);
-        this._map.on('mousemove', this._update, this);
-        this._map.on('mouseout', this._close, this);
-    },
-
-    removeHooks: function () {
-        this._map.off('mouseover', this._open, this);
-        this._map.off('mousemove', this._update, this);
-        this._map.off('mouseout', this._close, this);
-    },
-
-    _open: function (e) {
-        this._update(e);
-        this._popup.openOn(this._map);
-    },
-
-    _close: function () {
-        this._map.closePopup(this._popup);
-    },
-
-    _update: function (e) {
-        this._popup.setLatLng(e.latlng)
-            .setContent(e.latlng.toString());
-    }
-
-
-});
-
-L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);
-
 var bounds = [[0,0], [2000,2000]];
 
 var map = L.map('map', {
